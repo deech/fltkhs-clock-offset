caad5aefee33c7c080502c1664345f9f4584cc9c
Aditya Siram <aditya.siram@gmail.com>
2019-04-02 21:15:05 -0500

    Clean up and add offset.

--- app/Main.hs
+++ app/Main.hs
@@ -7,22 +7,15 @@ import Graphics.UI.FLTK.Theme.Light
 import Data.IORef
 import Data.Time.Clock.System(getSystemTime, SystemTime(..))
 import Foreign
+import Data.Text as T
 
-foo :: Ref Button -> IO ()
-foo _ = print "button callback"
-
-ui :: (?assets :: Assets) => IO ()
-ui = do
+offsetClock :: Rectangle -> Maybe T.Text -> Second -> IO (Ref Clock)
+offsetClock rect l (Second offset) = do
   timerRef <- newIORef Nothing
-  window <- windowNew
-            (Size (Width 600) (Height 400))
-            Nothing
-            Nothing
-  begin window
   let kickoff :: Ref Clock -> IO ()
       kickoff clockRef = do
         (MkSystemTime ss nss) <- getSystemTime
-        setValue clockRef (ClockSetSinceEpoch (ClockSinceEpoch (Second (fromIntegral ss))))
+        setValue clockRef (ClockSetSinceEpoch (ClockSinceEpoch (Second ((fromIntegral ss) + offset))))
         fp <- FL.addTimeout (schedule (fromIntegral nss / 1000)) (kickoff clockRef)
         writeIORef timerRef (Just fp)
       schedule micros =
@@ -34,32 +27,31 @@ ui = do
         case fpMaybe of
           Just fp -> do
             FL.removeTimeout fp
-            print fp
             freeHaskellFunPtr fp
-            print fp
             writeIORef timerRef Nothing
           Nothing -> return ()
-  c <- clockCustom
-        (Rectangle (Position (X 100) (Y 0)) (Size (Width 400) (Height 400)))
-        Nothing
-        (Just drawClock)
-        (Just
-           (defaultCustomWidgetFuncs
-             {
-               handleCustom =
-                 (Just (\clockRef event ->
-                           case event of
-                             FL.Show -> Right<$>kickoff clockRef
-                             FL.Hide -> Right<$>cleanup
-                             _ -> handleClockBase (safeCast clockRef) event)),
-               destroyCallbacksCustom =
-                 (Just (\clockRef callbacks -> cleanup >> defaultDestroyWidgetCallbacks clockRef callbacks))
-             }))
-  b <- buttonNew (toRectangle (10,10,100,50)) (Just "Button")
-  setCallback b foo
-  destroy b
-  b1 <- buttonNew (toRectangle (10,10,100,50)) (Just "Button")
-  setCallback b1 foo
+  clockCustom rect l (Just drawClock)
+    (Just
+       (defaultCustomWidgetFuncs
+         {
+           handleCustom =
+             (Just (\clockRef event ->
+                       case event of
+                         FL.Show -> Right<$>kickoff clockRef
+                         FL.Hide -> Right<$>cleanup
+                         _ -> handleClockBase (safeCast clockRef) event)),
+           destroyCallbacksCustom =
+             (Just (\clockRef callbacks -> cleanup >> defaultDestroyWidgetCallbacks clockRef callbacks))
+         }))
+
+ui :: (?assets :: Assets) => IO ()
+ui = do
+  window <- windowNew
+            (Size (Width 600) (Height 400))
+            Nothing
+            Nothing
+  begin window
+  _ <- offsetClock (Rectangle (Position (X 100) (Y 0)) (Size (Width 400) (Height 400))) Nothing (Second (-(60 * 60)))
   end window
   showWidget window
 
